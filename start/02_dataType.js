f8afef2be09abd6375d0e1e6881eabd3164099cf
PrinceMIT23 <149700947+PrinceMIT23@users.noreply.github.com>
2024-09-15 11:01:44 +0000

    data types

---
+++ start/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert( 3+3) we are using nodejs, not browser
+// code readablity should be high
+
+console.log("prince")
+let name = "prince"
+let age = 20
+let isLoggedIn = false
+
+
+
+// number => 2 to power 53
+//bigint
+// String => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
